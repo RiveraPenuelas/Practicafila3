4772565f980db9a0acb597ff7bad558a9d8bbff8
Jose Heras <josemanuelherascabrera@gmail.com>
2017-07-05 11:31:47 -0600

    agregue archivo jm

---
+++ master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Mayra Gabina Olague Lopez</title>
+	<link rel="stylesheet" href="{{asset("css/bootstrap.css")}}">
+	<script src="{{asset("js/jquery-3.2.1.js")}}"></script>
+</head>
+<body>
+
+<h1>Practica fila 3 </h1>
+</body>
+</html>
